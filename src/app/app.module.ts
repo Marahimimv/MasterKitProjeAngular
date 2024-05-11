86b5b72e7e4ea70720add2dee040e7837d0424a5
Faiqmzade3 <faiq.memmedzade3@gmail.com>
2024-05-11 16:18:11 +0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LatoutComponent } from './components/latout/latout.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './components/router';
+import { NavbarComponent } from './components/latout/navbar/navbar.component';
+import { FooterComponent } from './components/latout/footer/footer.component';
+import { HomeComponent } from './components/latout/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LatoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
